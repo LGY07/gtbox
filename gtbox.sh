d189b22791dd9eafc8277702cff27c045cf57028
UrsusFeline <84217988+LGY07@users.noreply.github.com>
2022-05-02 21:40:55 +0800

    .

---
+++ gtbox.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+less-help () {
+    echo -e "\e[34mGrass!-Toolbox\e[0m"
+    echo "usage: $0 [options] COMMAND"
+    echo ""
+    echo "-h    --help      display a helpful usage message"
+    echo ""
+    echo "This is an interactive script,if no args are added, the default interaction mode."
+}
+
+root () {
+    if [[ $(whoami) != "root" ]];then 
+    su -c "$0"
+    fi
+}
+
+help () {
+    echo -e "\e[34mBasic tools\e[0m"
+    echo ""
+    echo "help:"
+    echo "Show a helpful usage message."
+    echo "plugin-add [Package]:"
+    echo "Installing plug-ins from local files."
+    echo "plugin-maker [folders]:"
+    echo "Make a plugin package"
+    echo "plugin-update [plugin-name]"
+    echo "Update a plugin"
+    echo "plugin-config:"
+}
+
+plugin-add () {
+    echo -e "\e[34mInstall a plugin\e[0m"
+    echo -e "This command requires root privileges"
+    root && plugin-add
+    if test -f $1;then
+    tar -xf $1 ~/$1-cache
+    if [[ $(uname -m) == $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') || $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') == "all" ]];then echo "Check architecture [OK]";else echo "Check architecture [ERROR]" & exit 1;fi
+    if [[ $(uname -o) == $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') || $(grep "^OS-" ~/$1-cache/plugin.cfg | sed 's/OS-//') == "all" ]];then echo "Check OS [OK]";else echo "Check OS [ERROR]" & exit 1;fi
+    hash $(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//') > /dev/null 2>&1
+    if [ "$?" == "0" ]; then echo "Check shell [OK]" ;else echo "Check shell [ERROR]" && exit;fi
+    mkdir /opt/gtbox/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
+    echo "$(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//')">>/opt/gtbox/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')/shell
+    echo "$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//'):">>/opt/gtbox/toollist
+    echo "$(grep "^info-" ~/$1-cache/plugin.cfg | sed 's/info-//')">>/opt/gtbox/toollist
+    echo "$(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')#/opt/gtbox/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')/$(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')">>/opt/gtbox/startlist
+    cp ~/$1 /opt/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
+    rm -rf ~/$1
+}
+
+install-gtbox () {
+
+}
+
+ARGS () {
+
+}
+
+MAIN () {
+    echo -e "\e[34mGrass!-Toolbox\e[0m"
+    read -p "()" $__run
+    if [[ $(grep "^$__run#" /opt/gtbox/startlist | wc -l) -eq 1 ]];then
+    $(cat /opt/gtbox/$__run/shell) $(grep "^$__run#" /opt/gtbox/startlist | sed s/$__run#//)
+    else $__run
+    fi
+}
---
+++ plugin/README.md
@@ -0,0 +1,2 @@
+# 此目录为插件标准格式示例
+# This directory is an example of the standard format of the plug-in
