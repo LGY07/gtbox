8d66203a63d98f4a7a269e40b834feb515724653
UrsusFeline <84217988+LGY07@users.noreply.github.com>
2022-05-03 13:53:45 +0800

    Update gtbox.sh

--- gtbox.sh
+++ gtbox.sh
@@ -33,29 +33,61 @@ plugin-add () {
     echo -e "\e[34mInstall a plugin\e[0m"
     echo -e "This command requires root privileges"
     root && plugin-add
+    #Check file
     if test -f $1;then
     tar -xf $1 ~/$1-cache
+    else exit 1
+    fi
+    #Check architecture
+    if [[ $(uname -m) == $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') || $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') == "all" ]];then
+    echo -e "\033[32m[OK]\033[0m Check architecture"
+    else echo -e "\033[31m[ERROR]\033[0mCheck architecture" & exit 1
+    fi
+    #Check OS
+    if [[ $(uname -o) == $(grep "^OS-" ~/$1-cache/plugin.cfg | sed 's/OS-//') || $(grep "^OS-" ~/$1-cache/plugin.cfg | sed 's/OS-//') == "all" ]];then
+    echo -e "\033[32m[OK]\033[0m Check OS"
+    else echo -e "\033[31m[ERROR]\033[0mCheck OS" & exit 1
+    fi
 
-    $PKG_NAME = $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//')
-    $PKG_ARCHITECTURE = $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//')
-    
+    #Check Configuration
+    if [ $(grep "^name-" ~/$1-cache/plugin.cfg | wc -l) -ne 1 ];then echo -e "\033[31mThe configuration file is wrong\033[0m" & exit 1;fi
+    if [ $(grep "^info-" ~/$1-cache/plugin.cfg | wc -l) -ne 1 ];then echo -e "\033[31mThe configuration file is wrong\033[0m" & exit 1;fi
+    if [ $(grep "^shell-" ~/$1-cache/plugin.cfg | wc -l) -ne 1 ];then echo -e "\033[31mThe configuration file is wrong\033[0m" & exit 1;fi
+    if [ $(grep "^start-" ~/$1-cache/plugin.cfg | wc -l) -ne 1 ];then echo -e "\033[31mThe configuration file is wrong\033[0m" & exit 1;fi
 
-    if [[ $(uname -m) == $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') || $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') == "all" ]];then echo "Check architecture [OK]";else echo "Check architecture [ERROR]" & exit 1;fi
-    if [[ $(uname -o) == $(grep "^architecture-" ~/$1-cache/plugin.cfg | sed 's/architecture-//') || $(grep "^OS-" ~/$1-cache/plugin.cfg | sed 's/OS-//') == "all" ]];then echo "Check OS [OK]";else echo "Check OS [ERROR]" & exit 1;fi
-    hash $(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//') > /dev/null 2>&1
-    if [ "$?" == "0" ]; then echo "Check shell [OK]" ;else echo "Check shell [ERROR]" && exit;fi
-    mkdir /opt/gtbox/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
-    echo "$(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//')">>/opt/gtbox/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')/shell
-    echo "$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//'):">>/opt/gtbox/toollist
-    echo "$(grep "^info-" ~/$1-cache/plugin.cfg | sed 's/info-//')">>/opt/gtbox/toollist
-    echo "$(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')#/opt/gtbox/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')/$(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')">>/opt/gtbox/startlist
-    cp ~/$1-cache /opt/$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
+    #Vars
+    $PKG_NAME = $(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
+    $PKG_INFO = $(grep "^info-" ~/$1-cache/plugin.cfg | sed 's/info-//')
+    $PKG_SHELL = $(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//')
+    $PKG_START = $(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')
+
+    #Check name
+    if [ $(grep "^$PKG_NAME" /opt/gtbox/startlist | wc -l) -eq 1 ];then
+    echo -e "\033[31mPlugin name duplication\033[0m" & exit 1
+    fi
+    #Check shell
+    hash $PKG_SHELL > /dev/null 2>&1
+    if [ "$?" == "0" ];then
+    echo -e "\033[32m[OK]\033[0m Check shell"
+    else
+    echo -e "\033[31m[ERROR]\033[0mCheck shell"
+    echo -e "\033[31mPlease install $PKG_SHELL\033[0m"
+    exit 1
+    fi
+
+
+    #Move to /opt/gtbox/
+    mkdir /opt/gtbox/$PKG_NAME
+    cp ~/$1-cache /opt/$PKG_NAME
     rm -rf ~/$1-cache
+
+    #Add to list
+    echo "$PKG_SHELL">>/opt/gtbox/$PKG_NAME/shell.cfg
+    echo "$PKG_NAME">>/opt/gtbox/toollist
+    echo "$PKG_INFO">>/opt/gtbox/toollist
+    echo "$PKG_START">>/opt/gtbox/startlist
 }
 
-install-gtbox () {
-
-}
 
 ARGS () {
 
