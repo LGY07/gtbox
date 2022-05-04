0f41c806d0606529f83f2ccb2327c1469d87299b
UrsusFeline <84217988+LGY07@users.noreply.github.com>
2022-05-04 14:18:46 +0800

    Update gtbox.sh

--- gtbox.sh
+++ gtbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-less-help () {
+LESS_HELP () {
     echo -e "\e[34mGrass!-Toolbox\e[0m"
     echo "usage: $0 [options] COMMAND"
     echo ""
@@ -22,20 +22,24 @@ HELP () {
     echo ""
     echo "help:"
     echo "Show a helpful usage message."
-    echo "plugin-add [Package]:"
-    echo "Installing plug-ins from local files."
-    echo "plugin-maker [folders]:"
+    echo "plugin-add [package]:"
+    echo "Install plugins from local files."
+    echo "plugin-remove [plugin-name]"
+    echo "Removea plugin"
+    echo "plugin-update [plugin-name] [package]"
+    echo "Update or fix a plugin"
+    echo "plugin-make [folders]:"
     echo "Make a plugin package"
-    echo "plugin-update [plugin-name]"
-    echo "Update a plugin"
-    echo "plugin-config:"
+    echo ""
+    echo -e "\e[34mPlugin tools\e[0m"
+    cat /opt/gtbox/toollist
     return 0
 }
 
-plugin-add () {
+PLUGIN_ADD () {
     echo -e "\e[34mInstall a plugin\e[0m"
     echo -e "This command requires root privileges"
-    root && plugin-add
+    ROOT && PLUGIN_ADD $*
     #Check file
     if test -f $1;then
     tar -xf $1 ~/$1-cache
@@ -59,10 +63,10 @@ plugin-add () {
     if [[ $(grep "^start-" ~/$1-cache/plugin.cfg | wc -l) -ne 1 ]];then echo -e "\033[31mThe configuration file is wrong\033[0m" & exit 1;fi
 
     #Vars
-    $PKG_NAME = $(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
-    $PKG_INFO = $(grep "^info-" ~/$1-cache/plugin.cfg | sed 's/info-//')
-    $PKG_SHELL = $(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//')
-    $PKG_START = $(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')
+    PKG_NAME=$(grep "^name-" ~/$1-cache/plugin.cfg | sed 's/name-//')
+    PKG_INFO=$(grep "^info-" ~/$1-cache/plugin.cfg | sed 's/info-//')
+    PKG_SHELL=$(grep "^shell-" ~/$1-cache/plugin.cfg | sed 's/shell-//')
+    PKG_START=$(grep "^start-" ~/$1-cache/plugin.cfg | sed 's/start-//')
 
     #Check name
     if [ $(grep "^$PKG_NAME" /opt/gtbox/startlist | wc -l) -eq 1 ];then
@@ -85,17 +89,101 @@ plugin-add () {
     rm -rf ~/$1-cache
 
     #Add to list
-    echo "$PKG_NAME">>/opt/gtbox/toollist
-    echo "$PKG_INFO">>/opt/gtbox/toollist
+    echo "$PKG_NAME#$PKG_INFO">>/opt/gtbox/toollist
     echo "$PKG_NAME:$PKG_START">>/opt/gtbox/startlist
+    return 0
+}
+
+PLUGIN_REMOVE () {
+    echo -e "\e[34mRemove a plugin $1\e[0m"
+    ROOT && PLUGIN_REMOVE $*
+    if [[ 1 -gt $(grep "^$1:" /opt/gtbox/startlist | wc -l) ]];then echo "No such plugin" & exit 1;fi
+    read -p "[Y/n]" TRUE_FALSE
+    if [[ $TRUE_FALSE == "N" || $TRUE_FALSE == "n" ]];then exit 1
+    else
+    rm -rf /opt/gtbox/$1
+    sed -i /^$1/d /opt/gtbox/startlist
+    sed -i /^$1/d /opt/gtbox/toollist
+    fi
+    return 0
+}
+
+PLUGIN_UPDATE () {
+    echo -e "\e[34mUpdate a plugin $1\e[0m"
+    if [[ 1 -gt $(grep "^$1:" /opt/gtbox/startlist | wc -l) ]];then echo "No such plugin" & exit 1;fi
+    read -p "[Y/n]" TRUE_FALSE
+    if [[ $TRUE_FALSE == "N" || $TRUE_FALSE == "n" ]];then exit 1
+    else
+    PLUGIN-REMOVE $1 && PLUGIN_ADD $2
+    return 0
+}
+
+PLUGIN_MAKE () {
+    if test -d $1 ;then
+    echo -e "\e[34mMake a plugin package\e[0m"
+    else exit 1
+    fi
+    ROOT && PLUGIN_MAKE $*
+    echo -e "\e[36mMake a configuration file\e[0m"
+    echo "Cautions:"
+    echo "1.Please note the path,the plugin will be installed to /opt/gtbox/[plugin name]/"
+    echo "2.Plugin's name preferably without special characters and spaces."
+    echo "3.Introduction should be short"
+    #Basic Information
+    read -p "Plugin name        :" MAKE_PKG_NAME
+    read -p "Plugin introduction:" MAKE_PKG_INFO
+    #Architecture
+    echo "Select the architecture in which the plugin can run:"
+    echo "1:all"
+    echo "2:$(uname -m)"
+    echo "3:Other"
+    echo -e "\e[35m(default=1)\e[0m"
+    read -p "Enter the number:" MAKE_PKG_IN1
+    if [[ $MAKE_PKG_IN1 -eq 2 ]];then MAKE_PKG_UNAMEM=$(uname -m)
+    elif [[ $MAKE_PKG_IN1 -eq 3 ]];then read -p "Enter the architecture:" MAKE_PKG_UNAMEM
+    else MAKE_PKG_UNAMEM=all
+    #OS
+    echo "Select the OS in which the plugin can run:"
+    echo "1:all"
+    echo "2:$(uname -o)"
+    echo "3:Other"
+    echo -e "\e[35m(default=1)\e[0m"
+    read -p "Enter the number:" MAKE_PKG_IN2
+    if [[ $MAKE_PKG_IN2 -eq 2 ]];then MAKE_PKG_UNAMEM=$(uname -o)
+    elif [[ $MAKE_PKG_IN2 -eq 3 ]];then read -p "Enter the OS:" MAKE_PKG_UNAMEO
+    else MAKE_PKG_UNAMEM=all
+    #Shell
+    echo "Select the interpreter in which the plugin can run:"
+    echo "1:bash"
+    echo "2:Other"
+    echo -e "\e[35m(default=1)\e[0m"
+    read -p "Enter the number:" MAKE_PKG_IN3
+    elif [[ $MAKE_PKG_IN3 -eq 2 ]];then read -p "Enter the OS:" MAKE_PKG_SHELL
+    else MAKE_PKG_UNAMEM=bash
+    #Start command
+    echo "The script start command (absolute path), should be in the /opt/gtbox/$MAKE_PKG_NAME/ directory"
+    echo "For example:bash /opt/gtbox/$MAKE_PKG_NAME/start.sh"
+    read -p "Enter the start command:" MAKE_PKG_START
+    #Other info
+    echo "Enter some other information, such as the project URL"
+    read -p "Other info:" MAKE_PKG_OTHER
+    #Make
+    echo "name-$MAKE_PKG_NAME">$1/plugin.cfg
+    echo "info-$MAKE_PKG_INFO">>$1/plugin.cfg
+    echo "architecture-$MAKE_PKG_UNAMEM">>$1/plugin.cfg
+    echo "OS-$MAKE_PKG_UNAMEO">>$1/plugin.cfg
+    echo "sh-$MAKE_PKG_SHELL">>$1/plugin.cfg
+    echo "start-$MAKE_PKG_START">>$1/plugin.cfg
+    echo "#$MAKE_PKG_OTHER">>$1/plugin.cfg
+    tar -cJf $MAKE_PKG_NAME.tar.xz $1
     return 0
 }
 
 ARGS () {
     if [[ $1 == "--help" || $1 == "-h" ]];then
-    less-help
+    LESS_HELP
     fi
-    return 0
+    return 1
 }
 
 MAIN () {
@@ -104,15 +192,15 @@ MAIN () {
     do
     echo -e "\e[34mGrass!-Toolbox\e[0m"
     read -p "()" $__RUN
-    if [[ $(grep "^$__RUN#" /opt/gtbox/startlist | wc -l) -eq 1 ]];then
+    if [[ $(grep "^$__RUN:" /opt/gtbox/startlist | wc -l) -eq 1 ]];then
     $(grep "^$__RUN:" /opt/gtbox/startlist | sed s/$__RUN://)
-    elif [[ $__RUN = "help" ]];then
-    HELP
-    else $__RUN
+    else
+    $__RUN
     fi
     done
     return 0
 }
 
 ARGS $*
+if [[ $? = 1 ]];then exit 0;fi
 MAIN
