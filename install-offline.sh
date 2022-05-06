9a4cb3bc7f3c112b8fb62259fc5c4adfa8d92bb7
UrsusFeline <84217988+LGY07@users.noreply.github.com>
2022-05-07 00:00:44 +0800

    Update install-offline.sh

--- install-offline.sh
+++ install-offline.sh
@@ -3,8 +3,7 @@
 if [[ $(whoami) != "root" ]];then
 sudo bash $0
 fi
-mkdir /opt > /dev/null
-mkdir /opt/gtbox > /dev/null
+mkdir -p /opt/gtbox > /dev/null
 cp ./gtbox.sh /opt/gtbox/gtbox.sh
-chmod 555 /opt/gtbox/gtbox.sh
+chmod 555 /opt/gtbox/
 ln -sf /opt/gtbox/gtbox.sh /usr/bin/gtbox
